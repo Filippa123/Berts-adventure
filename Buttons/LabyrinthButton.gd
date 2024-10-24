8e81a418b022b6cf19267893115c47e5a3054380
Flip <71626431+Filippa123@users.noreply.github.com>
2024-10-24 10:39:37 +0300

    Initial commit

---
+++ Buttons/LabyrinthButton.gd
@@ -0,0 +1,17 @@
+extends TextureButton
+
+func _ready():
+	var button = Button.new()
+	button.text = "Labyrinth"
+	button.pressed.connect(self._button_pressed)
+	add_child(button)
+
+#var simultaneous_scene = preload().instantiate() # annat sätt o göra
+func _button_pressed():
+	print("Labyrith button pressed")
+	#get_tree().root.add_child(simultaneous_scene)	#annat sätt o göra
+	get_tree().change_scene_to_file("res://Levels/labyrinth_level.tscn")
+	
+
+
+
---
+++ Characters/player_bert.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@export var move_speed : float = 100
+
+
+func _physics_process(_delta):
+	# Get input direction
+	var input_direction = Vector2(
+		Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	) #down is positive, negative value means going up
+	#print(input_direction)
+	# update velocity
+	velocity = input_direction * move_speed
+	
+	# Move and slide 
+	move_and_slide()
