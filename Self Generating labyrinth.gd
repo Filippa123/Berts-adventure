0ecf8962b3ccc0fc0cabc5d2448c160b92e68af0
Flip <71626431+Filippa123@users.noreply.github.com>
2024-11-26 12:20:15 +0200

    Working on self gen labyrinth
    Worked on adding code for selfgenerating labyrinth. does not work yet

--- Self Generating labyrinth.gd
+++ Self Generating labyrinth.gd
@@ -1,4 +1,23 @@
 extends Node2D
 
-const maze = preload("res://LabyrinthGen.gd")
+#main.gd 
+const maze = preload("res://Constructs/labyrinth_self_gen_tile_map.tscn")
 
+var hud_enabled = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+#Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("show_hud"):
+		hud_enabled = not hud_enabled
+		$"CanvasLayer".set_visible(hud_enabled)
+
+	if Input.is_action_just_pressed("reset"):
+		var new_maze = maze.instantiate()
+		var existing_maze = get_children().filter(func(x):
+			return "TileMap" in x.name)[0]
+		existing_maze.queue_free()
+		add_child(new_maze)
