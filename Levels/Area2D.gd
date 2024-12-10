b8f0855cb602f2afaa5996482d1a5a9723351399
Flip <71626431+Filippa123@users.noreply.github.com>
2024-12-10 19:40:05 +0200

    Working on new win screen
    Added new win screen picture, chnaged so new node is added not changed to

--- Levels/Area2D.gd
+++ Levels/Area2D.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
+var simultaneous_scene = preload("res://Levels/win_screen.tscn").instantiate()
 
 func _on_body_entered(body):
 	print("WIN :D") # Replace with function body.
 #adda vid behov en if som kollar att de e bert o int vad som helst
 	if body.is_in_group("Player"):
 		print("Win :D (bert)")
-		get_tree().change_scene_to_file("res://Levels/win_screen.tscn")
+		#get_tree().change_scene_to_file("res://Levels/win_screen.tscn")
+		get_tree().root.add_child(simultaneous_scene)
