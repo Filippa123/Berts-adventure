2056a76f5be1751f14595093cd970fa915bf2b19
Flip <71626431+Filippa123@users.noreply.github.com>
2024-12-11 15:57:07 +0200

    Adding more levels

---
+++ Levels/next_button.gd
@@ -0,0 +1,7 @@
+extends Button
+
+func _button_pressed():
+	print("menu button pressed")
+	#get_tree().root.add_child(simultaneous_scene)	#annat sätt o göra
+	#get_tree().change_scene_to_file("res://Levels/labyrinth_level.tscn")
+	#get_tree().change_scene_to_file("res://Levels/self_generating_labyrinth.tscn")
